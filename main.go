b5bc40f37a3cbf6846a8f77b66c2067f1e15156c
nikhitha44 <angaranikhitha44@gmail.com>
2024-09-26 11:59:23 -0400

    add binary

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	Greeting()
 	advanceGreeting("Nikhitha")
-	advanceGreeting("Harika")
 
-}
-
-func Greeting() {
-	fmt.Println("Hey There!")
-}
-
-func advanceGreeting(name string) {
-	fmt.Printf(" Hello, %s!\n", name)
 }
